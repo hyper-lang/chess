001dd4e124823558f76db4e024a1ed63ef2c0c86
hyper-lang <zhw@groundworksdesign.com>
2026-05-02 16:50:50 -0600

    Added the translate helper function to ChessBoard. Changed the board from a 2D to a 1D array. Added equals and hashcode funcitons for ChessBoard.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Arrays;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -7,14 +9,26 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
-    private ChessPosition[][] board = new ChessPosition[7][7];
+    private ChessPosition[] board;
 
     public ChessBoard() {
+        board = new ChessPosition[64];
         for(int i = 1; i < 9; i++){
             for(int j = 1; j < 9; j++){
-                board[]
+                board[translate(i, j)] = new ChessPosition(i, j);
             }
         }
+    }
+
+    /**
+     * Translates 2D coordinates to the 1D array
+     * 
+     * @param i the row index
+     * @param j the column index
+     * @return the 1D board array index
+     */
+    private int translate(int i, int j){
+        return ((i - 1) * 8) + (j - 1);
     }
 
     /**
@@ -24,7 +38,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        board[translate(position.getRow(), position.getColumn())].setPiece(piece);
     }
 
     /**
@@ -35,7 +49,21 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return board[translate(position.getRow(), position.getColumn())].getPiece();
+    }
+
+    /**
+     * Sets 4 corners of a rectangle to a certain piece based on it's quadrant 3 position (helper function for resetBoard)
+     * 
+     * @param i the row coordinate for the piece
+     * @param j the column coordinate for the piece
+     * @param piece the piece to set
+     */
+    private void setRectangle(int i, int j, ChessPiece.PieceType piecetype){
+        board[translate(i, j)].setPiece(new ChessPiece(ChessGame.TeamColor.WHITE, piecetype));
+        board[translate(9 - i, j)].setPiece(new ChessPiece(ChessGame.TeamColor.WHITE, piecetype));
+        board[translate(i, 9 - j)].setPiece(new ChessPiece(ChessGame.TeamColor.BLACK, piecetype));
+        board[translate(9 - i, 9 - j)].setPiece(new ChessPiece(ChessGame.TeamColor.BLACK, piecetype));
     }
 
     /**
@@ -43,6 +71,27 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + Arrays.hashCode(board);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ChessBoard other = (ChessBoard) obj;
+        if (!Arrays.equals(board, other.board))
+            return false;
+        return true;
     }
 }
