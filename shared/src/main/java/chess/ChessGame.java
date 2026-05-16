a67a998aed64ce61263e77246ff129bf9fb21499
hyper-lang <zhw@groundworksdesign.com>
2026-05-16 17:21:18 -0600

    Added brackets to improve readability in the equals and hashcode methods of ChessGame

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -241,20 +241,26 @@ public class ChessGame {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj){
             return true;
-        if (obj == null)
+        }
+        if (obj == null){
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (getClass() != obj.getClass()){
             return false;
+        }
         ChessGame other = (ChessGame) obj;
         if (board == null) {
-            if (other.board != null)
+            if (other.board != null){
                 return false;
-        } else if (!board.equals(other.board))
+            }
+        } else if (!board.equals(other.board)){
             return false;
-        if (turn != other.turn)
+        }
+        if (turn != other.turn){
             return false;
+        }
         return true;
     }
 
