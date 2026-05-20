04b8f5f9f833b69555169b8f0c78fe4ee5c694e8
hyper-lang <zhw@groundworksdesign.com>
2026-05-19 19:45:05 -0600

    Wrote out MemoryUserDAO.java, fleshed out UserService.java

--- server/src/main/java/dataaccess/MemoryUserDAO.java
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -1,5 +1,22 @@
 package dataaccess;
 
+import model.UserData;
+import java.util.Map;
+import java.util.HashMap;
+
 public class MemoryUserDAO implements UserDAO {
-    
+    private Map<String, String[]> users = new HashMap<>();
+
+    public void createUser(UserData user){
+        users.put(user.username(), new String[]{user.email(), user.password()});
+    }
+
+    public UserData getUser(String username){
+        String[] data = users.get(username);
+        return new UserData(username, data[0], data[1]);
+    }
+
+    public void clear(){
+        users = new HashMap<>();
+    }
 }
---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,20 @@
+package service;
+
+import model.UserData;
+import dataaccess.*;
+
+public class UserService {
+    private UserDAO userDAO;
+    private AuthDAO authDAO;
+
+    public UserService(UserDAO userDAO, AuthDAO authDAO){
+        this.userDAO = userDAO;
+        this.authDAO = authDAO;
+    }
+
+    public AuthData register(UserData user){
+        userDAO.createUser();
+    }
+    public AuthData login(){}
+    public void logout(String authToken){}
+}
