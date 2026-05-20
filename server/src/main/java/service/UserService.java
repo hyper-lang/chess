b1e01a573343f7040fd2c50fa700b8d145f4d916
hyper-lang <zhw@groundworksdesign.com>
2026-05-19 20:18:11 -0600

    Wrote out the UserService service

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,22 +1,33 @@
 package service;
 
+import model.AuthData;
 import model.UserData;
 import dataaccess.*;
+import java.util.UUID;
 
 public class UserService {
     private UserDAO userDAO;
     private AuthDAO authDAO;
+
+    public static String generateToken() {
+        return UUID.randomUUID().toString();
+    }
 
     public UserService(UserDAO userDAO, AuthDAO authDAO){
         this.userDAO = userDAO;
         this.authDAO = authDAO;
     }
 
-    public AuthData register(UserData user){
+    public AuthData register(UserData user) throws DataAccessException{
         userDAO.createUser(user);
+        return login(user);
     }
 
-    public AuthData login(){}
-    
+    public AuthData login(UserData user) throws DataAccessException{
+        AuthData authData = new AuthData(generateToken(), user.username());
+        authDAO.createAuth(authData);
+        return authData;
+    }
+
     public void logout(String authToken){}
 }
