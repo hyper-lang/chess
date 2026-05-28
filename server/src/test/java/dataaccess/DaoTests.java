6ff18f565ce249c7668c76033cdd2b2be4dd9780
hyper-lang <zhw@groundworksdesign.com>
2026-05-27 20:37:47 -0600

    Wrote tests until createAuthPositive in DaoTests

--- server/src/test/java/dataaccess/DaoTests.java
+++ server/src/test/java/dataaccess/DaoTests.java
@@ -9,6 +9,7 @@ public class DaoTests {
     @Test
     public void createUserPositive() throws Exception {
         UserDAO userDAO = new DatabaseUserDAO();
+        userDAO.clear();
         UserData cosmo = new UserData("cosmo", "couger", "cosmo@byu.edu");
         userDAO.createUser(cosmo);
 
@@ -18,6 +19,7 @@ public class DaoTests {
     @Test
     public void createUserNegative() throws Exception {
         UserDAO userDAO = new DatabaseUserDAO();
+        userDAO.clear();
         UserData shane = new UserData("shanereese", "gocougs", "shanereese@byu.edu");
         UserData shane2 = new UserData("shanereese", "gocougs", "shanereese@byu.edu");
 
@@ -27,50 +29,115 @@ public class DaoTests {
     }
 
     @Test
-    public void getUserPositive() throws Exception{}
+    public void getUserPositive() throws Exception{
+        UserDAO userDAO = new DatabaseUserDAO();
+        userDAO.clear();
+
+        UserData cosmo = new UserData("cosmo", "couger", "cosmo@byu.edu");
+        userDAO.createUser(cosmo);
+
+        UserData shane = new UserData("shanereese", "gocougs", "shanereese@byu.edu");
+        userDAO.createUser(shane);
+
+        assertNotNull(userDAO.getUser("cosmo"));
+        assertNotNull(userDAO.getUser("shane"));
+    }
 
     @Test
-    public void getUserNegative() throws Exception{}
+    public void getUserNegative() throws Exception{
+        UserDAO userDAO = new DatabaseUserDAO();
+        userDAO.clear();
+
+        assertThrows(DataAccessException.class, () -> userDAO.getUser("cosmo"));
+    }
 
     @Test
-    public void createAuthPositive() throws Exception{}
+    public void createAuthPositive() throws Exception{
+        AuthDAO authDAO = new DatabaseAuthDAO();
+        authDAO.clear();
+
+        AuthData testauth = new AuthData("testToken", "cosmo");
+        authDAO.createAuth(testauth);
+
+        assertNotNull(authDAO.getAuth("testToken"));
+
+    }
 
     @Test
-    public void createAuthNegative() throws Exception{}
+    public void createAuthNegative() throws Exception{
+        AuthDAO authDAO = new DatabaseAuthDAO();
+        authDAO.clear();
+    }
 
     @Test
-    public void getAuthPositive() throws Exception{}
+    public void getAuthPositive() throws Exception{
+        AuthDAO authDAO = new DatabaseAuthDAO();
+        authDAO.clear();
+    }
 
     @Test
-    public void getAuthNegative() throws Exception{}
+    public void getAuthNegative() throws Exception{
+        AuthDAO authDAO = new DatabaseAuthDAO();
+        authDAO.clear();
+    }
 
     @Test
-    public void deleteAuthPositive() throws Exception{}
+    public void deleteAuthPositive() throws Exception{
+        AuthDAO authDAO = new DatabaseAuthDAO();
+        authDAO.clear();
+    }
 
     @Test
-    public void deleteAuthNegative() throws Exception{}
+    public void deleteAuthNegative() throws Exception{
+        AuthDAO authDAO = new DatabaseAuthDAO();
+        authDAO.clear();
+    }
 
     @Test
-    public void createGamePositive() throws Exception{}
+    public void createGamePositive() throws Exception{
+        GameDAO gameDAO = new DatabaseGameDAO();
+        gameDAO.clear();
+    }
 
     @Test
-    public void createGameNegative() throws Exception{}
+    public void createGameNegative() throws Exception{
+        GameDAO gameDAO = new DatabaseGameDAO();
+        gameDAO.clear();
+    }
 
     @Test
-    public void getGamePositive() throws Exception{}
+    public void getGamePositive() throws Exception{
+        GameDAO gameDAO = new DatabaseGameDAO();
+        gameDAO.clear();
+    }
 
     @Test
-    public void getGameNegative() throws Exception{}
+    public void getGameNegative() throws Exception{
+        GameDAO gameDAO = new DatabaseGameDAO();
+        gameDAO.clear();
+    }
 
     @Test
-    public void listGamesPositive() throws Exception{}
+    public void listGamesPositive() throws Exception{
+        GameDAO gameDAO = new DatabaseGameDAO();
+        gameDAO.clear();
+    }
 
     @Test
-    public void listGamesNegative() throws Exception{}
+    public void listGamesNegative() throws Exception{
+        GameDAO gameDAO = new DatabaseGameDAO();
+        gameDAO.clear();
+    }
 
     @Test
-    public void updateGamePositive() throws Exception{}
+    public void updateGamePositive() throws Exception{
+        GameDAO gameDAO = new DatabaseGameDAO();
+        gameDAO.clear();
+    }
 
     @Test
-    public void updateGameNegative() throws Exception{}
+    public void updateGameNegative() throws Exception{
+        GameDAO gameDAO = new DatabaseGameDAO();
+        gameDAO.clear();
+    }
 }
