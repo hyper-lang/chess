8861fd2e610e0327e6784f5bf8c2acca22e9a1bf
hyper-lang <zhw@groundworksdesign.com>
2026-06-01 23:48:40 -0600

    Wrote out PostLoginClient cases

--- client/src/main/java/client/PostloginClient.java
+++ client/src/main/java/client/PostloginClient.java
@@ -1,26 +1,55 @@
 package client;
 
 import model.AuthData;
+import model.GameData;
+
+import java.util.Collection;
 
 public class PostloginClient {
     private ServerFacade server;
     private AuthData auth;
+    private String help;
 
     public PostloginClient(String url, AuthData auth){
         server = new ServerFacade(url);
         this.auth = auth;
+        help = """
+                create <NAME>
+                list
+                join <ID> [WHITE|BLACK]
+                observe <ID>
+                logout
+                quit
+                help
+                """;
     }
 
-    public boolean postLoginInput(String input){
+    public boolean postLoginInput(String input) throws Exception {
         String[] words = input.split("\\s+");
 
-        if(words[0].toLowerCase().equals("create")){}
-        else if(words[0].toLowerCase().equals("list")){}
-        else if(words[0].toLowerCase().equals("join")){}
-        else if(words[0].toLowerCase().equals("observe")){}
-        else if(words[0].toLowerCase().equals("logout")){}
-        else if(words[0].toLowerCase().equals("quit")){}
-        else if(words[0].toLowerCase().equals("help")){}
+        if(words[0].toLowerCase().equals("create")){
+            int gameID = server.createGame(auth, words[0]);
+            System.out.println("Created game number " + gameID);
+        } else if(words[0].toLowerCase().equals("list")){
+            Collection<GameData> games = server.listGames(auth);
+            int index = 1;
+            for(GameData i : games){
+                System.out.println(index + " " + i.gameName());
+                index += 1;
+            }
+        } else if(words[0].toLowerCase().equals("join")){
+            int gameID = Integer.parseInt(words[1]);
+            String color = words[2].toUpperCase();
+            server.joinGame(auth, color, gameID);
+        } else if(words[0].toLowerCase().equals("observe")){
+            int gameID = Integer.parseInt(words[1]);
+            //rest to be implemented in phase 6
+        } else if(words[0].toLowerCase().equals("logout")){
+            server.logout(auth);
+            return false;
+        } else if(words[0].toLowerCase().equals("help")){
+            System.out.println(help);
+        }
         return true;
     }
 }
