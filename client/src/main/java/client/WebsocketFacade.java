8fedb6d32df777588772f8c83fe3893190f357dc
hyper-lang <zhw@groundworksdesign.com>
2026-06-07 19:02:28 -0600

    Built basic WebSocketFacade for the client

--- client/src/main/java/client/WebsocketFacade.java
+++ client/src/main/java/client/WebsocketFacade.java
@@ -1,5 +1,33 @@
 package client;
 
-public class WebsocketFacade {
-    
+import jakarta.websocket.ContainerProvider;
+import jakarta.websocket.Endpoint;
+import jakarta.websocket.EndpointConfig;
+import jakarta.websocket.MessageHandler;
+import jakarta.websocket.Session;
+import jakarta.websocket.WebSocketContainer;
+
+import java.net.URI;
+import java.util.Scanner;
+
+public class WebsocketFacade extends Endpoint {
+    public Session session;
+
+    public WebsocketFacade(String url) throws Exception {
+        URI uri = new URI(url);
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        session = container.connectToServer(this, uri);
+
+        this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+            public void onMessage(String message) {
+                System.out.println(message);
+            }
+        });
+    }
+
+    public void send(String message) throws Exception {
+        session.getBasicRemote().sendText(message);
+    }
+
+    public void onOpen(Session session, EndpointConfig endpointConfig){}
 }
